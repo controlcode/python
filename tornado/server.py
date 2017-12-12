c9deab3d989b64138120dabd44db90a4065d3d0e
Richard Weeks <richard.weeks@live.co.uk>
2017-12-12 11:34:04 +0000

    Tornado

---
+++ tornado/server.py
@@ -0,0 +1,17 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self):
+        self.write("Hello, world")
+        self.finish()
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8000)
+    tornado.ioloop.IOLoop.instance().start()
